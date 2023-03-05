ad47be4c6c8b679d93bacafa8aa1d333fe78e4c1
Miguel Henriquez <mig.henriquezp@gmail.com>
2023-03-05 02:09:01 -0300

    Footer added

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,9 +1,75 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '../styles/footer.module.css';
+
 
 export const Footer = () => {
   return (
-    <footer>
-      <div>Footer</div>
+    <footer className={styles.footer}>
+
+      <h2 className={styles.footerTitle}>F-Code</h2>
+
+      <div className={styles.medias}>
+        <Link
+          className={styles.media}
+          href='https://facebook.com'
+          target='blank'
+        >
+          <Image
+            className={styles.mediaIcon}
+            src={require(`/public/media/facebook.svg`)}
+            alt='Facebook'
+          />
+        </Link>
+
+        <Link
+          className={styles.media}
+          href='https://twitter.com'
+          target='blank'
+        >
+          <Image
+            className={styles.mediaIcon}
+            src={require(`/public/media/twitter.svg`)}
+            alt='Twitter'
+          />
+        </Link>
+
+        <Link
+          className={styles.media}
+          href='https://instagram.com'
+          target='blank'
+        >
+          <Image
+            className={styles.mediaIcon}
+            src={require(`/public/media/instagram.svg`)}
+            alt='Instagram'
+          />
+        </Link>
+      </div>
+
+      <div className={styles.terms}>
+        <Link
+          className={styles.termLink}
+          href={'/terms/terms_of_use'}>
+          Terms of use
+        </Link>
+        <br />
+        <Link
+          className={styles.termLink}
+          href={'/terms/privacy_policy'}>
+          Privacy policy
+        </Link>
+      </div>
+
+      <hr className={styles.hr} />
+      <p className={styles.copyright}>
+        2023 F-Code
+        <br />
+        <span>All rights reserved</span>
+      </p>
+
+
     </footer>
   );
 }
