15263ba1d35989650d98a7b98649912743a280e7
MHP24 <mig.henriquezp@gmail.com>
2023-05-26 16:51:47 -0400

    loader profile page fix

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -45,9 +45,6 @@ const ProfilePage: NextPage = () => {
     })();
   }, [session, userQuery]);
 
-
-  console.log({ profileData });
-
   return (
     <MainLayout
       pageDescription={`FCode ${profileData.data?.profile.username}' s profile`}
@@ -55,88 +52,90 @@ const ProfilePage: NextPage = () => {
 
     >
       {
-        profileData.data && !profileData.isLoading ?
-          <div className={`${styles.profileContainer} animate__animated animate__fadeIn`}>
-            <h2 className={styles.profileTitle}>{`${profileData.data.profile.username}'s performance`}</h2>
-            <section className={styles.section}>
-              <div className={styles.profileHeaderContainer}>
-                <div className={`${styles.profileItem} ${styles.profileUser}`}>
-                  <Image
-                    className={styles.profilePicture}
-                    src={
-                      profileData.data.profile.picture === 'no-picture' ?
-                        '/pictures/no-picture.png'
-                        : profileData.data.profile.picture
+        !profileData.isLoading && (
+          profileData.data ?
+            <div className={`${styles.profileContainer} animate__animated animate__fadeIn`}>
+              <h2 className={styles.profileTitle}>{`${profileData.data.profile.username}'s performance`}</h2>
+              <section className={styles.section}>
+                <div className={styles.profileHeaderContainer}>
+                  <div className={`${styles.profileItem} ${styles.profileUser}`}>
+                    <Image
+                      className={styles.profilePicture}
+                      src={
+                        profileData.data.profile.picture === 'no-picture' ?
+                          '/pictures/no-picture.png'
+                          : profileData.data.profile.picture
+                      }
+                      alt={profileData.data.profile.username}
+                      width={220}
+                      height={220}
+                    />
+
+                    <Button
+                      size={0.9}
+                      text='Sign out'
+                      fn={() => logoutUser()}
+                      w={150}
+                    />
+                  </div>
+
+                  <div className={`${styles.profileItem} ${styles.profileProgress}`}>
+                    {
+                      (
+                        profileData.data.profile.technologies
+                          .challengeDistribution
+                      ).map((tech, i) => {
+                        return (
+                          <LanguageProgress
+                            key={`language-progress-${i}-${tech.language}`}
+                            {...tech}
+                          />
+                        )
+                      })
                     }
-                    alt={profileData.data.profile.username}
-                    width={220}
-                    height={220}
-                  />
-
-                  <Button
-                    size={0.9}
-                    text='Sign out'
-                    fn={() => logoutUser()}
-                    w={150}
-                  />
-                </div>
-
-                <div className={`${styles.profileItem} ${styles.profileProgress}`}>
-                  {
-                    (
-                      profileData.data.profile.technologies
-                        .challengeDistribution
-                    ).map((tech, i) => {
-                      return (
-                        <LanguageProgress
-                          key={`language-progress-${i}-${tech.language}`}
-                          {...tech}
-                        />
-                      )
-                    })
-                  }
-                </div>
-              </div>
-            </section>
-
-            <hr className={styles.separator} />
-
-            <section className={styles.section}>
-              <div className={styles.profileRanking}>
-                <LeaderboardCard
-                  userScore={profileData.data.profile.ranking.userScore}
-                  rank={profileData.data.profile.ranking.rank}
-                  title={'Ranked stats'}
-                />
-              </div>
-            </section>
-
-            <hr className={styles.separator} />
-
-            {
-              profileData.data.profile.recentActivity &&
-
-              <section className={`${styles.section} ${styles.activitySection}`}>
-                <h3 className={styles.title}>Recent Activity</h3>
-
-                <div className={styles.recentChallengesGrid}>
-                  {
-                    profileData.data.profile.recentActivity.map((challenge, i) => {
-                      return <ChallengeProfile
-                        key={`recent-challenge-profile-${i}`}
-                        {...challenge}
-                      />
-                    })
-                  }
+                  </div>
                 </div>
               </section>
-            }
-          </div>
-          :
-          <div className={styles.noUser}>
-            <p className={styles.notFound}>{'404'}</p>
-            <p className={styles.noUserText}>{`We couldn't find the user`}</p>
-          </div>
+
+              <hr className={styles.separator} />
+
+              <section className={styles.section}>
+                <div className={styles.profileRanking}>
+                  <LeaderboardCard
+                    userScore={profileData.data.profile.ranking.userScore}
+                    rank={profileData.data.profile.ranking.rank}
+                    title={'Ranked stats'}
+                  />
+                </div>
+              </section>
+
+              <hr className={styles.separator} />
+
+              {
+                profileData.data.profile.recentActivity &&
+
+                <section className={`${styles.section} ${styles.activitySection}`}>
+                  <h3 className={styles.title}>Recent Activity</h3>
+
+                  <div className={styles.recentChallengesGrid}>
+                    {
+                      profileData.data.profile.recentActivity.map((challenge, i) => {
+                        return <ChallengeProfile
+                          key={`recent-challenge-profile-${i}`}
+                          {...challenge}
+                        />
+                      })
+                    }
+                  </div>
+                </section>
+              }
+            </div>
+            :
+            <div className={styles.noUser}>
+              <p className={styles.notFound}>{'404'}</p>
+              <p className={styles.noUserText}>{`We couldn't find the user`}</p>
+            </div>
+        )
       }
 
     </MainLayout>
