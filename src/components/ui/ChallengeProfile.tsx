7ce77ea19647d46709ce3382621bcc7a9f3bdc07
MHP24 <mig.henriquezp@gmail.com>
2023-05-31 15:07:17 -0400

    Challenge profile recent activity card not challenge fixed

--- src/components/ui/ChallengeProfile.tsx
+++ src/components/ui/ChallengeProfile.tsx
@@ -28,24 +28,26 @@ interface Props {
 
 export const ChallengeProfile: FC<Props> = ({ updatedAt, challenge }) => {
   return (
-    <Link
-      className={styles.challengeProfile}
-      href={`/challenges/${challenge.language}/${challenge.slug}`}
-      target='_blank'
-    >
-      <Image
-        className={styles.challengeImages}
-        src={`/techs/${challenge.language}.svg`}
-        width={35}
-        height={35}
-        alt={challenge.language}
+    challenge && (
+      <Link
+        className={styles.challengeProfile}
+        href={`/challenges/${challenge.language}/${challenge.slug}`}
+        target='_blank'
+      >
+        <Image
+          className={styles.challengeImages}
+          src={`/techs/${challenge.language}.svg`}
+          width={35}
+          height={35}
+          alt={challenge.language}
 
-      />
+        />
 
-      <p className={styles.challengeData}>{challenge._id}</p>
-      <p className={styles.challengeData}>{challenge.slug.replace(/\_+/gi, ' ').replace(/^\w/gi, w => w.toLocaleUpperCase())}</p>
-      <div className={`${styles.difficulty} ${difficulties[challenge.difficulty].class}`}></div>
-      <p className={`${styles.challengeData} ${styles.date}`}>{`${new Date(updatedAt).toUTCString().split(' ').slice(0, -1).join(' ')}`}</p>
-    </Link>
+        <p className={styles.challengeData}>{challenge._id}</p>
+        <p className={styles.challengeData}>{challenge.slug.replace(/\_+/gi, ' ').replace(/^\w/gi, w => w.toLocaleUpperCase())}</p>
+        <div className={`${styles.difficulty} ${difficulties[challenge.difficulty].class}`}></div>
+        <p className={`${styles.challengeData} ${styles.date}`}>{`${new Date(updatedAt).toUTCString().split(' ').slice(0, -1).join(' ')}`}</p>
+      </Link>
+    )
   );
 }
