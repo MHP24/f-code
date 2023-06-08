1e92913d8f321cb95539343371c3819fe5603a42
MHP24 <mig.henriquezp@gmail.com>
2023-06-08 12:32:02 -0400

    fix add modal portal method

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,4 +1,5 @@
-import React, { Dispatch, FC, PropsWithChildren, SetStateAction, useState } from 'react';
+import ReactDOM from 'react-dom';
+import React, { Dispatch, FC, PropsWithChildren, SetStateAction } from 'react';
 import styles from '../styles/modal.module.css';
 import { Logo } from '.';
 import Image from 'next/image';
@@ -16,37 +17,40 @@ export const Modal: FC<PropsWithChildren<Props>> =
     { open, setModal, children }
   ) => {
 
+    const portalRoot = document.getElementById('root-modal');
+
     return (
       <>
         {
-          open &&
-          <div className={styles.modal}>
-            <div className={`${styles.modalContent} animate__animated animate__zoomIn`}>
-              <button
-                className={styles.closeBtn}
-                onClick={() => setModal((prev) => ({ ...prev, isOpen: false }))}
-              >
-                <Image
-                  src={'/illustrations/close.svg'}
-                  alt='close'
-                  width={30}
-                  height={30}
-                />
-              </button>
-              <div className={styles.modalHeader}>
-                <Logo
-                  isPrimary={false}
-                  column
-                  size={50}
-                />
-              </div>
+          open && typeof window !== 'undefined' &&
+          ReactDOM.createPortal(
+            <div className={styles.modal}>
+              <div className={`${styles.modalContent} animate__animated animate__zoomIn`}>
+                <button
+                  className={styles.closeBtn}
+                  onClick={() => setModal((prev) => ({ ...prev, isOpen: false }))}
+                >
+                  <Image
+                    src={'/illustrations/close.svg'}
+                    alt='close'
+                    width={30}
+                    height={30}
+                  />
+                </button>
+                <div className={styles.modalHeader}>
+                  <Logo
+                    isPrimary={false}
+                    column
+                    size={50}
+                  />
+                </div>
 
-              <div className={styles.modalBody}>
-                {children}
-              </div>
+                <div className={styles.modalBody}>
+                  {children}
+                </div>
 
-            </div>
-          </div >
+              </div>
+            </div >, portalRoot!)
         }
       </>
     );
